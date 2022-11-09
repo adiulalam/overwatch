5507eed2a793dd1d60ba7e8533d24105b0fc3095
Adiul Alam Adil <adiulalam@gmail.com>
2022-11-09 20:46:32 +0000

    expo migration

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
 import { Text, View } from "react-native";
+import "./main.css";
 
 export default function App() {
   return (
