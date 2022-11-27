5bd5c59426fc3506e3515ac6e5c390326299a73e
Adiul Alam Adil <adiulalam@gmail.com>
2022-11-27 16:52:56 +0000

    push carousel

---
+++ os/multi-platform/components/carouselPush.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Dimensions, Platform, View } from "react-native";
+import Carousel from "react-native-reanimated-carousel";
+import { useSharedValue } from "react-native-reanimated";
+import { SBItem } from "./SBItem";
+
+export const CarouselPush = () => {
+	const progressValue = useSharedValue(0);
+
+	const window =
+		Platform.OS === "web" && Dimensions.get("window").width > Dimensions.get("window").height
+			? {
+					width: Dimensions.get("window").height * 0.6,
+					height: Dimensions.get("window").height * 0.6 * 0.56,
+					parallaxScrollingOffset: Dimensions.get("window").height * 0.6 * 0.12,
+			  }
+			: {
+					width: Dimensions.get("window").width,
+					height: Dimensions.get("window").width * 0.6,
+					parallaxScrollingOffset: Dimensions.get("window").width * 0.12,
+			  };
+
+	const colors = ["#26292E", "#899F9C", "#B3C680", "#5C6265", "#F5D399", "#F1F1F1"];
+
+	return (
+		<Carousel
+			width={window.width}
+			height={window.height}
+			loop
+			pagingEnabled={true}
+			snapEnabled={true}
+			onProgressChange={(_, absoluteProgress) => (progressValue.value = absoluteProgress)}
+			mode="parallax"
+			modeConfig={{
+				parallaxScrollingScale: 0.9,
+				parallaxScrollingOffset: window.parallaxScrollingOffset,
+			}}
+			data={colors}
+			renderItem={({ index }) => <SBItem index={index} />}
+		/>
+	);
+};
