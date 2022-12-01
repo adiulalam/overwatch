b7807a65046778c61401615cbfa4f2c0ef5cb807
Adiul Alam Adil <adiulalam@gmail.com>
2022-12-01 15:11:58 +0000

    maps

--- data/overwatch.sql
+++ data/overwatch.sql
@@ -17,3 +17,19 @@ CREATE TABLE overwatch.ability (
 	FOREIGN KEY (fk_hero_uuid ) REFERENCES overwatch.hero (hero_uuid)
 );
 
+CREATE TABLE overwatch.map_mode (
+	map_mode_uuid UUID NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
+	type text NOT NULL,
+	description text NOT NULL,
+	index integer NOT NULL,
+	fk_hero_uuid UUID NOT NULL,
+	FOREIGN KEY (fk_hero_uuid ) REFERENCES overwatch.hero (hero_uuid)
+);
+
+CREATE TABLE overwatch.map (
+	map_uuid UUID NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
+	name text NOT NULL,
+	map_image text NOT NULL,
+	fk_map_mode_uuid UUID NOT NULL,
+	FOREIGN KEY (fk_map_mode_uuid ) REFERENCES overwatch.map_mode (map_mode_uuid)
+);
