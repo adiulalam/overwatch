e19dc21e8bf161972705654ef82548c73195b27b
Adiul Alam Adil <adiulalam@gmail.com>
2022-12-11 01:07:20 +0000

    heroes edit component

--- os/web/admin/admin.jsx
+++ os/web/admin/admin.jsx
@@ -3,6 +3,7 @@ import { createMaterialTopTabNavigator } from "@react-navigation/material-top-ta
 import Ionicons from "@expo/vector-icons/Ionicons";
 const { data } = require("./data.json");
 import tw from "twrnc";
+import { useEffect, useState } from "react";
 
 //todo implement deep-diff and _.differenceWith
 
@@ -28,25 +29,59 @@ function SettingsScreen() {
 	);
 }
 
-const HeroesScreen = () => {
+const HeroesScreen = ({ navigation, route }) => {
+	const [heroData, setHeroData] = useState({ ...route?.params?.hero } ?? {});
+
+	useEffect(() => {
+		setHeroData({ ...route?.params?.hero });
+		return () => heroData;
+	}, [route?.params?.hero]);
+
 	return (
-		<View style={tw`flex flex-col items-center p-2`}>
-			<View style={tw`flex flex-row md:grid md:grid-cols-5 w-1/3 mb-6 space-x-1.5 `}>
-				<View style={tw`flex justify-center md:sm:justify-end `}>
-					<Text style={tw`block text-white font-bold md:text-right mb-1 md:mb-0 px-4`} for="inline-full-name">
-						Heroes!
-					</Text>
-				</View>
-				<View style={tw`flex col-span-3 w-full`}>
-					<TextInput
-						value="text"
-						style={tw`bg-gray-200 appearance-none border-2 ${
-							// value
-							false ? "" : "border-red-500"
-						} rounded w-full py-2 px-4 text-gray-700 leading-normal focus:outline-none focus:bg-white focus:border-purple-500`}
-					></TextInput>
-				</View>
-			</View>
+		<View style={tw`flex flex-col items-center justify-center p-2`}>
+			{Object?.keys(heroData ?? {})?.map((element, i) =>
+				element === "abilities" ? null : element === "hero_uuid" ? (
+					<View key={i} style={tw`flex flex-row w-1/3 mb-6 `}>
+						<View style={tw`flex justify-center md:sm:justify-end `}>
+							<Text
+								style={tw` text-white font-bold md:text-right mb-1 md:mb-0 px-4`}
+								for="inline-full-name"
+							>
+								{element}
+							</Text>
+						</View>
+						<View style={tw`flex w-full`}>
+							<TextInput
+								value={heroData[element]}
+								style={tw`bg-gray-200 border-2 ${
+									// value
+									false ? "" : "border-red-500"
+								} rounded w-full py-2 px-4 text-gray leading-normal focus:outline-none focus:bg-white focus:border-purple-500`}
+							></TextInput>
+						</View>
+					</View>
+				) : (
+					<View key={i} style={tw`flex flex-row w-1/3 mb-6`}>
+						<View style={tw`flex justify-center md:sm:justify-end `}>
+							<Text
+								style={tw` text-white font-bold md:text-right mb-1 md:mb-0 px-4`}
+								for="inline-full-name"
+							>
+								{element}
+							</Text>
+						</View>
+						<View style={tw`flex w-full`}>
+							<TextInput
+								value={heroData[element]}
+								style={tw`bg-gray-200 border-2 ${
+									// value
+									false ? "" : "border-red-500"
+								} rounded w-full py-2 px-4 text-gray leading-normal focus:outline-none focus:bg-white focus:border-purple-500`}
+							></TextInput>
+						</View>
+					</View>
+				)
+			)}
 		</View>
 	);
 };
@@ -54,7 +89,7 @@ const HeroesScreen = () => {
 const HerosTab = createMaterialTopTabNavigator();
 const Heroes = ({ navigation, route }) => {
 	const heroes = route?.params?.Heroes;
-	console.log(heroes);
+	// console.log(heroes);
 	return (
 		<HerosTab.Navigator
 			screenOptions={{ tabBarItemStyle: { width: 80 }, tabBarScrollEnabled: true, swipeEnabled: true }}
