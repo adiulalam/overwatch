abff213e83b7d8ca02e40e00fc4727433c54dc2c
Adiul Alam Adil <adiulalam@gmail.com>
2022-12-11 00:35:29 +0000

    template

--- os/web/admin/admin.jsx
+++ os/web/admin/admin.jsx
@@ -1,7 +1,8 @@
-import { View, Text } from "react-native";
+import { View, Text, TextInput } from "react-native";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import Ionicons from "@expo/vector-icons/Ionicons";
 const { data } = require("./data.json");
+import tw from "twrnc";
 
 //todo implement deep-diff and _.differenceWith
 
@@ -29,8 +30,23 @@ function SettingsScreen() {
 
 const HeroesScreen = () => {
 	return (
-		<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-			<Text style={{ color: "white" }}>Heroes!</Text>
+		<View style={tw`flex flex-col items-center p-2`}>
+			<View style={tw`flex flex-row md:grid md:grid-cols-5 w-1/3 mb-6 space-x-1.5 `}>
+				<View style={tw`flex justify-center md:sm:justify-end `}>
+					<Text style={tw`block text-white font-bold md:text-right mb-1 md:mb-0 px-4`} for="inline-full-name">
+						Heroes!
+					</Text>
+				</View>
+				<View style={tw`flex col-span-3 w-full`}>
+					<TextInput
+						value="text"
+						style={tw`bg-gray-200 appearance-none border-2 ${
+							// value
+							false ? "" : "border-red-500"
+						} rounded w-full py-2 px-4 text-gray-700 leading-normal focus:outline-none focus:bg-white focus:border-purple-500`}
+					></TextInput>
+				</View>
+			</View>
 		</View>
 	);
 };
